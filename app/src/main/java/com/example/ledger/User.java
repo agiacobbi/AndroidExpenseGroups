9ad21df38895bf92c4770214898634ffe44aff03
JCAASSHH <jalentacsiat@gmail.com>
2019-11-30 17:02:37 -0800

    user class

--- app/src/main/java/com/example/ledger/User.java
+++ app/src/main/java/com/example/ledger/User.java
@@ -11,4 +11,20 @@ public class User {
         this.username = paramUsername;
         this.name = paramName;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
