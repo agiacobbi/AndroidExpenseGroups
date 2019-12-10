f4634b6223e03bf1c342ab3171fc020b42d82430
JCAASSHH <jalentacsiat@gmail.com>
2019-12-10 14:52:37 -0800

    added new activities and some layouts

---
+++ app/src/main/java/com/example/ledger/ViewAllGroupsActivity.java
@@ -0,0 +1,14 @@
+package com.example.ledger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAllGroupsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_groups);
+    }
+}
