29608293fe8210d5db49c6fd5e940c1ba9981227
JCAASSHH <jalentacsiat@gmail.com>
2019-12-11 00:01:00 -0800

    start on join group

--- app/src/main/java/com/example/ledger/JoinGroupActivity.java
+++ app/src/main/java/com/example/ledger/JoinGroupActivity.java
@@ -3,12 +3,29 @@ package com.example.ledger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class JoinGroupActivity extends AppCompatActivity {
+    EditText inputGroupId = (EditText)findViewById(R.id.groupId);
+    Button joinGroupButton = (Button)findViewById(R.id.joinGroup);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_group);
+        joinGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(inputGroupId.getText().toString().equals("")){
+                    Toast.makeText(JoinGroupActivity.this, "Please enter a groupId", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    
+                }
+            }
+        });
     }
 }
