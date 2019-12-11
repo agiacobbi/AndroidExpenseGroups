9cd5639fa0ccd79f024e5c68d1381b61ca9203ce
Alex Giacobbi <agiacobbi@zagmail.gonzaga.edu>
2019-12-11 13:18:48 -0800

    fixed quit bug

--- app/src/main/java/com/example/ledger/MainActivity.java
+++ app/src/main/java/com/example/ledger/MainActivity.java
@@ -317,15 +317,10 @@ public class MainActivity extends AppCompatActivity {
             else if (resultCode == Activity.RESULT_CANCELED) {
                 // they backed out of the sign in activity
                 // let's exit
-                finish();
+                //finish();
             }
         }
-        //Does not work after user signs in
-        //trying to get data from cost
-        /*if(data != null){
-            newCost = (Cost)data.getSerializableExtra("cost");
-            Log.d(TAG, "Cost amount: " + newCost.getAmountCost()+ " Description: " + newCost.getCostDescription());
-        }*/
+
     }
 }
 
